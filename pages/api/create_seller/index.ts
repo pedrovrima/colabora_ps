c3441377aec367945440e63946c707ee0e71f6a2
Pedro Martins <pedrovrima@gmail.com>
2022-08-06 22:31:21 -0300

    final crate seller

--- pages/api/create_seller/index.ts
+++ pages/api/create_seller/index.ts
@@ -1,13 +1,19 @@
 import { prisma } from "../../../db";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-
-export default function createSale(req: NextApiRequest, res: NextApiResponse) {
-  console.log("hi")
-    const { seller } = req.body;
-
-  prisma.seller.create({
-    data: { ...seller },
-  });
-  res.send(200)
+export default async function createSale(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("hi");
+  const { seller } = req.body;
+  try {
+    await prisma.seller.create({
+      data: { ...seller },
+    });
+    res.send(200);
+  } catch (e) {
+    console.log(e);
+    res.send(500)
+  }
 }
