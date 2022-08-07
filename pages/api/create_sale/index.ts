f9ea35a04f387e6d4ed3c1fb882d9fc725604d86
Pedro Martins <pedrovrima@gmail.com>
2022-08-06 22:31:05 -0300

    final create sale

--- pages/api/create_sale/index.ts
+++ pages/api/create_sale/index.ts
@@ -2,12 +2,15 @@ import { prisma } from "../../../db";
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 
-export default function createSale(req: NextApiRequest, res: NextApiResponse) {
+export default async function createSale(req: NextApiRequest, res: NextApiResponse) {
   console.log("hi")
     const { sale } = req.body;
-
-  prisma.sales.create({
+try{
+  await prisma.sales.create({
     data: { ...sale, productSales: { create: sale.productSales } },
   });
-  res.send(200)
+  res.send(200)}catch(e){
+    console.log(e)
+    res.send(500)
+  }
 }
